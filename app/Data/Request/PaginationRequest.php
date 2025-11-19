ea7628fb769ad748689d070816c17c89c801ffe3
Verrandy Bagus <verrandyb@gmail.com>
2025-11-19 22:39:03 +0800

    fix

---
+++ app/Data/Request/PaginationRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Data\Request;
+
+use Spatie\LaravelData\Data;
+
+class PaginationRequest extends Data
+{
+    public function __construct(
+        public ?int $limit = 10,
+        public ?string $cursor = null,
+        public ?string $search = null
+    ) {}
+}
+
+// rename
